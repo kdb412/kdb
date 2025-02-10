ff8753052c6ca0b8d75fa599188af4ed1da3542d
Irfan <14016443+icode99@users.noreply.github.com>
2025-02-09 20:04:57 -0500

    update

--- kdb.h
+++ kdb.h
@@ -8,48 +8,67 @@
 #include <fstream>
 
 namespace kdb {
+  using std::string;
+  using std::ofstream;
+  using std::ifstream;
 
-using std::string;
-using std::ofstream;
+  class db {
+  private:
+    ofstream ofs;
+    static constexpr int MAX_BLOCK_ALLOC = 0x64;
 
- class db {
+    struct _db_header {
+      char dbid[1];
+      char magic[4];
+      char version[4];
+      char oname[100];
+      char kek[512];
+      int64_t idate;
+    };
 
-   private:
-     struct _db_header{
-       char dbid[4];
-       char magic[4];
-       char version[4];
-       char oname[100];
-       char kek[512];
-       int64_t idate;
-     };
+    struct _db_block {
+      char blkid[1];
+      char dbid[2];
+      char flid[30];
+      char flname[100];
+      char unused[4096 - 133];
+    } kdb_blk;
 
-     const string db_path;
-     bool  active;
-     ofstream odbf;
 
-     bool init() {
-       odbf.open(db_path);
-       if(odbf.is_open())
-         return true;
-       return false;
-     }
+    const string db_path;
+    bool active;
+    ofstream odbf;
+    ifstream idbf;
 
-   public:
-     db(const db&) = delete;
-     db& operator=(const db&) = delete;
-     db(db&&) = delete;
-     db& operator=(db&&) = delete;
+    bool init() {
+      odbf.open(db_path, std::ios::binary | std::ios::app);
+      idbf.open(db_path, std::ios::binary | std::ios::in);
 
-     db(string path = "file.db") : db_path(path), active(false), odbf() {
-       active = init();
-     }
+      if (odbf.is_open() && idbf.is_open())
+        return true;
+      else {
+        odbf.close();
+        idbf.close();
+      }
 
-     bool is_open(){ return active; }
+      return false;
+    }
 
-     // todo: read() / write() / delete() / find()
+  public:
+    db(const db &) = delete;
 
- };
+    db &operator=(const db &) = delete;
 
+    db(db &&) = delete;
+
+    db &operator=(db &&) = delete;
+
+    db(string path = "file.db") : db_path(path), active(false), odbf(), idbf() {
+      active = init();
+    }
+
+    bool is_open() { return active; }
+
+    // todo: read() / write() / delete() / find()
+  };
 }
-
